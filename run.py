8435dd18aa3a5df954f49bafa5a262001c2b9063
Volker Schmidt <volker.schmidt@stud.uni-hannover.de>
2021-09-14 02:31:23 +0200

    include sc2 library

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from datetime import datetime
 # Load bot
 from zerg import ZergAI
 
-bot = Bot(Race.Zerg, ZergAI(game_step = 1), 'SunTzu')
+bot = Bot(Race.Zerg, ZergAI(game_step = 4), 'SunTzu')
 
 # Start game
 if __name__ == "__main__":
@@ -25,7 +25,7 @@ if __name__ == "__main__":
         replayPath = os.path.join("C:\\Users", "Ryzen", "Documents", "StarCraft II", "Accounts", "490189910", "2-S2-1-9348025", "Replays", "Multiplayer", time + ".SC2Replay")
         opponent = Computer(Race.Protoss, Difficulty.VeryHard, ai_build=AIBuild.Macro)
         sc2.run_game(
-            sc2.maps.get("LightShadeLE"),
+            sc2.maps.get("LightShadeAIE"),
             [bot, opponent],
             realtime=False,
             save_replay_as=replayPath,
