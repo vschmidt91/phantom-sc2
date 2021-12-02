929503e8a368f2804cf3a4a3f8257c029389c94c
Volker Schmidt <volker.schmidt@stud.uni-hannover.de>
2021-12-02 06:12:23 +0100

    fix: scout overlord distance

--- suntzu/zerg.py
+++ suntzu/zerg.py
@@ -436,7 +436,7 @@ class ZergAI(CommonAI):
 
         overlord_targets = list()
         if not self.bases[-2].taken_since:
-            target = self.bases[-2].position.towards(self.game_info.map_center, 12.75)
+            target = self.bases[-2].position.towards(self.game_info.map_center, 12)
         else:
             enemy_location = self.enemy_start_locations[0]
             enemy_main_ramp = min(
