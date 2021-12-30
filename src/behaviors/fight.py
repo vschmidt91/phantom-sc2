01740a0d6244dec03df28c052281177a685f0927
Volker Schmidt <volker.schmidt@stud.uni-hannover.de>
2021-12-30 15:48:46 +0100

    fix retreat out of bounds

--- src/behaviors/fight.py
+++ src/behaviors/fight.py
@@ -75,6 +75,8 @@ class FightBehavior(UnitBehavior):
         return advantage
 
     def get_path_towards(self, unit: Unit, target: Point2) -> Point2:
+        a = self.ai.game_info.playable_area
+        target = np.clip(target, (a.x, a.y), (a.right, a.top))
         if unit.is_flying:
             enemy_map = self.ai.enemy_vs_air_map
         else:
