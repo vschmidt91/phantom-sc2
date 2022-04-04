fix: drones fight if no townhall; 0 dps units

src/ai_base.py
@@ -1040,7 +1040,7 @@ class AIBase(ABC, BotAI):
         def add_unit_to_map(map: np.ndarray, unit: Unit) -> np.ndarray:
             radius = unit.radius + max(unit.ground_range, unit.air_range)
             if radius == 0:
-                return radius
+                return map
             dps = max(unit.ground_dps, unit.air_dps)
             weight = dps / (math.pi * radius**2)
             # if weight < 1: