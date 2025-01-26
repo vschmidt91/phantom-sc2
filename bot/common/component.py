91699a59b9a205fe1808c43de4ff2f4ccbf09d99
Volker Schmidt <volker.schmidt91@gmail.com>
2025-01-26 18:20:58 +0100

    Revert "test: add river training on_step"
    This reverts commit 630cd5b017e66c0bae467ece6df14caefcc998ab.

--- bot/common/component.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-
-class Component[TObservation, TState, TAction](ABC):
-
-    @abstractmethod
-    def step(self, observation: TObservation, state: TState) -> tuple[TState, TAction]:
-        pass
