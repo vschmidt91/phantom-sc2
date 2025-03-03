6d38a5b73923424c5085320c5e7976e8f00d8a31
Volker Schmidt <volker.schmidt91@gmail.com>
2025-03-03 03:58:00 +0100

    reduce number of compiled problems

--- scripts/compile_cvxpy.py
+++ scripts/compile_cvxpy.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     output_dir = "bin"
 
     with tempfile.TemporaryDirectory() as temp_dir:
-        for log_n in range(3, 9):
+        for log_n in range(4, 8):
 
             n = 2 ** log_n
             m = n
