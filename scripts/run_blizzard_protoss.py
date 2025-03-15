87d3535fbe91da6418eb4161770b2bc7efa2bf0e
Volker Schmidt <volker.schmidt91@gmail.com>
2025-03-15 05:07:48 +0100

    Add docker test game action

---
+++ scripts/run_blizzard_protoss.py
@@ -0,0 +1,22 @@
+import sys
+from os import path
+
+sys.path.append(path.join(path.dirname(__file__), ".."))
+
+import sc2
+from sc2 import Race, Difficulty
+from sc2.player import Bot, Computer
+
+from Main import RoachRush
+bot = Bot(Race.Zerg, RoachRush())
+
+# Start game
+if __name__ == "__main__":
+    sc2.run_game(
+        sc2.maps.get("AcolyteLE"),
+        [
+            bot,
+            Computer(Race.Protoss, Difficulty.Hard),
+        ],
+        realtime=False,
+    )
