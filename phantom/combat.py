6828b1685c59b0aacd5bac5a43b879d9677fb2dc
Volker Schmidt <volker.schmidt91@gmail.com>
2025-11-11 01:02:15 +0100

    simulation based combat, fix retreat state

--- phantom/combat.py
+++ phantom/combat.py
@@ -17,7 +17,6 @@ from sc2.ids.unit_typeid import UnitTypeId
 from sc2.ids.upgrade_id import UpgradeId
 from sc2.position import Point2
 from sc2.unit import Unit
-from sc2.units import Units
 
 from phantom.common.action import Action, Attack, HoldPosition, Move, UseAbility
 from phantom.common.constants import COMBATANT_STRUCTURES, HALF
@@ -25,7 +24,6 @@ from phantom.common.distribute import distribute
 from phantom.common.graph import graph_components
 from phantom.common.utils import (
     calculate_dps,
-    can_attack,
     pairwise_distances,
     sample_bilinear,
     structure_perimeter,
@@ -38,78 +36,17 @@ from phantom.observation import Observation
 class CombatPrediction:
     outcome: EngagementResult
     outcome_for: Mapping[int, EngagementResult]
-    attacking: Mapping[int, float]
 
 
 class CombatState:
     def __init__(self, bot: AresBot, knowledge: Knowledge) -> None:
         self.bot = bot
         self.knowledge = knowledge
-        self.contact_range_internal = 7
-        self.contact_range = 14
         self.is_attacking = set[int]()
+        self.retreat_to_creep = False
 
     def step(self, observation: Observation) -> "CombatAction":
         return CombatAction(self, observation)
-
-    def _predict_trivial(self, units: Sequence[Unit], enemy_units: Sequence[Unit]) -> EngagementResult | None:
-        if not any(units) and not any(enemy_units):
-            return EngagementResult.TIE
-        elif not any(units):
-            return EngagementResult.LOSS_OVERWHELMING
-        elif not any(enemy_units):
-            return EngagementResult.VICTORY_OVERWHELMING
-        return None
-
-    def _predict(self, units: Units, enemy_units: Units) -> CombatPrediction:
-        if trivial := self._predict_trivial(units, enemy_units):
-            return CombatPrediction(trivial, {}, {})
-
-        all_units = [*units, *enemy_units]
-        positions = [u.position for u in units]
-        enemy_positions = [u.position for u in enemy_units]
-
-        distance_matrix = pairwise_distances(positions, enemy_positions)
-        contact = distance_matrix < self.contact_range
-        for (i, unit), (j, enemy_unit) in product(enumerate(units), enumerate(enemy_units)):
-            if not can_attack(unit, enemy_unit) and not can_attack(unit, enemy_unit):
-                contact[i, j] = False
-
-        contact_own = np.zeros((len(units), len(units)))
-        contact_enemy = pairwise_distances(enemy_positions) < self.contact_range_internal
-        adjacency_matrix = np.block([[contact_own, contact], [contact.T, contact_enemy]])
-
-        components = graph_components(adjacency_matrix)
-
-        simulator_kwargs = dict(
-            good_positioning=False,
-            workers_do_no_damage=False,
-        )
-        outcome = self.bot.mediator.can_win_fight(
-            own_units=units, enemy_units=enemy_units, timing_adjust=False, **simulator_kwargs
-        )
-
-        outcome_for = dict[int, EngagementResult]()
-        attacking = dict[int, float]()
-        for component in components:
-            local_units = [all_units[i] for i in component]
-            local_own = [u for u in local_units if u.is_mine]
-            local_enemies = [u for u in local_units if u.is_enemy]
-            local_outcome = self._predict_trivial(local_own, local_enemies) or self.bot.mediator.can_win_fight(
-                own_units=local_own,
-                enemy_units=local_enemies,
-                timing_adjust=True,
-                **simulator_kwargs,
-            )
-            enemy_outcome = EngagementResult(10 - local_outcome.value)
-            local_attacking = sum(1 for u in local_own if u.tag in self.is_attacking) / len(units)
-            for u in local_own:
-                outcome_for[u.tag] = local_outcome
-                attacking[u.tag] = local_attacking
-            for u in local_enemies:
-                outcome_for[u.tag] = enemy_outcome
-
-        return CombatPrediction(outcome, outcome_for, attacking)
 
 
 class CombatAction:
@@ -154,6 +91,9 @@ class CombatAction:
             COMBATANT_STRUCTURES
         )
 
+        self.time_to_kill = self._time_to_kill(self.combatants, self.enemy_combatants)
+        self.time_to_attack = self._time_to_attack(self.combatants, self.enemy_combatants)
+
         self.retreat_targets = np.atleast_2d(retreat_targets).astype(int)
 
         self.retreat_air = cy_dijkstra(
@@ -183,19 +123,78 @@ class CombatAction:
         else:
             self.runby_pathing = None
 
-        self.prediction = self.state.bot.mediator.can_win_fight(
-            own_units=self.combatants,
-            enemy_units=self.enemy_combatants,
-            timing_adjust=False,
-        )
-
-        self.retreat_to_creep = False
+        self.prediction = self.predict()
 
         if self.observation.knowledge.enemy_race not in {Race.Zerg, Race.Random}:
-            if self.prediction <= EngagementResult.TIE:
-                self.retreat_to_creep = True
-            elif self.prediction >= EngagementResult.VICTORY_DECISIVE:
-                self.retreat_to_creep = False
+            if self.prediction.outcome <= EngagementResult.LOSS_MARGINAL:
+                self.state.retreat_to_creep = True
+            elif self.prediction.outcome >= EngagementResult.VICTORY_CLOSE:
+                self.state.retreat_to_creep = False
+
+    def _predict_trivial(self, units: Sequence[Unit], enemy_units: Sequence[Unit]) -> EngagementResult | None:
+        if not any(units) and not any(enemy_units):
+            return EngagementResult.TIE
+        elif not any(units):
+            return EngagementResult.LOSS_OVERWHELMING
+        elif not any(enemy_units):
+            return EngagementResult.VICTORY_OVERWHELMING
+        return None
+
+    def predict(self) -> CombatPrediction:
+        units = self.combatants
+        enemy_units = self.enemy_combatants
+
+        if trivial := self._predict_trivial(units, enemy_units):
+            return CombatPrediction(trivial, {})
+
+        # time_to_attack = self._time_to_attack(units, enemy_units)
+        time_to_attack = self.time_to_attack
+        enemy_time_to_attack = self._time_to_attack(enemy_units, units)
+
+        # time_to_kill = self._time_to_kill(units, enemy_units)
+        time_to_kill = self.time_to_kill
+        enemy_time_to_kill = self._time_to_kill(enemy_units, units)
+
+        contact = (time_to_attack < 3) & (time_to_kill < np.inf)
+        enemy_contact = (enemy_time_to_attack < 3) & (enemy_time_to_kill < np.inf)
+
+        contact_symmetrical = contact | enemy_contact.T
+
+        contact_internal = np.zeros((len(units), len(units)))
+        enemy_contact_internal = np.zeros((len(enemy_units), len(enemy_units)))
+        adjacency_matrix = np.block(
+            [[contact_internal, contact_symmetrical], [contact_symmetrical.T, enemy_contact_internal]]
+        )
+
+        components = graph_components(adjacency_matrix)
+
+        simulator_kwargs = dict(
+            good_positioning=False,
+            workers_do_no_damage=True,
+        )
+        outcome = self.state.bot.mediator.can_win_fight(
+            own_units=units, enemy_units=enemy_units, timing_adjust=False, **simulator_kwargs
+        )
+
+        all_units = [*units, *enemy_units]
+        outcome_for = dict[int, EngagementResult]()
+        for component in components:
+            local_units = [all_units[i] for i in component]
+            local_own = [u for u in local_units if u.is_mine]
+            local_enemies = [u for u in local_units if u.is_enemy]
+            local_outcome = self._predict_trivial(local_own, local_enemies) or self.state.bot.mediator.can_win_fight(
+                own_units=local_own,
+                enemy_units=local_enemies,
+                timing_adjust=True,
+                **simulator_kwargs,
+            )
+            enemy_outcome = EngagementResult(10 - local_outcome.value)
+            for u in local_own:
+                outcome_for[u.tag] = local_outcome
+            for u in local_enemies:
+                outcome_for[u.tag] = enemy_outcome
+
+        return CombatPrediction(outcome, outcome_for)
 
     def retreat_with(self, unit: Unit, limit=3) -> Action | None:
         retreat_map = self.retreat_air if unit.is_flying else self.retreat_ground
@@ -275,45 +274,10 @@ class CombatAction:
         if unit.type_id in {UnitTypeId.BANELING}:
             return Move(target.position)
 
-        if not unit.is_flying and self.retreat_to_creep and not self.observation.creep[p]:
+        if not unit.is_flying and self.state.retreat_to_creep and not self.observation.creep[p]:
             return self.retreat_with(unit)
 
-        # simulate battle
-        c = 0.1
-        eps = 1e-3
-        a = 0.0
-        alpha = 0.0
-        for u in self.combatants:
-            d = u.distance_to(target) - u.radius - target.radius
-            d -= cy_range_vs_target(unit=u, target=target)
-            dt = max(0, d) / max(eps, u.movement_speed)
-            w = 1 / (1 + c * dt**2)
-            a += w
-            alpha += w * (u.health + u.shield) * (u.air_dps if target.is_flying else u.ground_dps)
-        alpha /= a
-
-        b = 0.0
-        beta = 0.0
-        for u in self.enemy_combatants:
-            d = u.distance_to(unit) - u.radius - unit.radius
-            d -= cy_range_vs_target(unit=u, target=unit)
-            dt = max(0, d) / max(eps, u.movement_speed)
-            w = 1 / (1 + c * dt**2)
-            b += w
-            beta += w * (u.health + u.shield) * (u.air_dps if unit.is_flying else u.ground_dps)
-        beta /= b
-
-        lancester_power = 1.5
-        lancester_a = alpha * (a**lancester_power)
-        lancester_b = beta * (b**lancester_power)
-        if lancester_a > lancester_b:
-            a_final = ((lancester_a - lancester_b) / alpha) ** (1 / lancester_power)
-            outcome = a_final / a
-        else:
-            b_final = ((lancester_b - lancester_a) / beta) ** (1 / lancester_power)
-            outcome = -b_final / b
-
-        # outcome = self.prediction.outcome_for[unit.tag]
+        outcome = self.prediction.outcome_for[unit.tag]
 
         # outcome_state = self.state.outcome_state.setdefault(unit.tag, 0.0)
         # outcome_state += .1 * (outcome - outcome_state)
@@ -323,17 +287,13 @@ class CombatAction:
             self.state.bot.mediator.get_air_grid if unit.is_flying else self.state.bot.mediator.get_ground_grid
         )
 
-        if outcome > 1 / 3:
+        if outcome >= EngagementResult.VICTORY_CLOSE:
             self.state.is_attacking.add(unit.tag)
-        elif outcome < -1 / 3:
+        elif outcome <= EngagementResult.LOSS_MARGINAL:
             self.state.is_attacking.discard(unit.tag)
 
         if unit.tag in self.state.is_attacking:
             should_runby = not self.observation.creep[p]
-            if unit.distance_to(self.observation.start_location) < unit.distance_to(
-                self.observation.knowledge.enemy_start_locations[0]
-            ):
-                should_runby = False
 
             if should_runby and unit.can_attack_ground and runby_target:
                 return Attack(runby_target)
@@ -371,15 +331,7 @@ class CombatAction:
             return None
         return UseAbility(AbilityId.BURROWDOWN)
 
-    def _targeting_cost(self, units: Sequence[Unit], enemies: Sequence[Unit]) -> np.ndarray:
-        distances = pairwise_distances(
-            [u.position for u in units],
-            [u.position for u in enemies],
-        )
-        movement_speed = np.array([u.movement_speed for u in units])
-        movement_speed = np.repeat(movement_speed[:, np.newaxis], len(enemies), axis=1)
-        time_to_attack = np.divide(distances, movement_speed)
-
+    def _time_to_kill(self, units: Sequence[Unit], enemies: Sequence[Unit]) -> np.ndarray:
         ground_dps = np.array([u.ground_dps for u in units])
         air_dps = np.array([u.air_dps for u in units])
 
@@ -397,30 +349,41 @@ class CombatAction:
         enemy_hp = np.repeat(enemy_hp[np.newaxis, :], len(units), axis=0)
 
         time_to_kill = np.divide(enemy_hp, dps)
+        return time_to_kill
 
-        cost = time_to_attack + time_to_kill
+    def _time_to_attack(self, units: Sequence[Unit], enemies: Sequence[Unit]) -> np.ndarray:
+        ground_range = np.array([u.ground_range for u in units])
+        air_range = np.array([u.air_range for u in units])
+        radius = np.array([u.radius for u in units])
+        enemy_radius = np.array([u.radius for u in enemies])
 
-        # def cost_fn(a: Unit, b: Unit, d: float) -> float:
-        #     if a.order_target == b.tag and can_attack(a, b):
-        #         return 0.0
-        #     r = cy_range_vs_target(unit=a, target=b)
-        #     travel_distance = max(0.0, d - a.radius - b.radius - r)
-        #     time_to_reach = np.divide(travel_distance, a.movement_speed)
-        #     dps = calculate_dps(a, b)
-        #     if (b.is_burrowed or b.is_cloaked) and not self.observation.bot.mediator.get_is_detected(
-        #         unit=b, by_enemy=False
-        #     ):
-        #         dps = 0.0
-        #     time_to_kill = np.divide(b.health + b.shield, dps)
-        #     random_offset = hash((a.tag, b.tag)) / (2**sys.hash_info.width)
-        #     return time_to_reach + time_to_kill + 1e-10 * random_offset
-        #
-        # cost = np.array(
-        #     [[cost_fn(ai, bj, distances[i, j]) for j, bj in enumerate(enemies)] for i, ai in enumerate(units)]
-        # )
+        def is_attackable(u: Unit) -> bool:
+            if u.is_burrowed or u.is_cloaked:
+                return self.observation.bot.mediator.get_is_detected(unit=u, by_enemy=u.is_mine)
+            return True
 
-        cost = np.nan_to_num(cost, nan=np.inf)
-        return cost
+        enemy_attackable = np.array([1.0 if is_attackable(u) else 0.0 for u in enemies])
+        enemy_flying = np.array([1.0 if u.is_flying else 0.0 for u in enemies])
+        enemy_ground = 1.0 - enemy_flying
+
+        ranges = np.outer(ground_range, enemy_attackable * enemy_ground) + np.outer(
+            air_range, enemy_attackable * enemy_flying
+        )
+
+        distances = pairwise_distances(
+            [u.position for u in units],
+            [u.position for u in enemies],
+        )
+        distances -= ranges
+        distances -= np.repeat(radius[:, np.newaxis], len(enemies), axis=1)
+        distances -= np.repeat(enemy_radius[np.newaxis, :], len(units), axis=0)
+        distances = np.maximum(distances, 0.0)
+
+        movement_speed = np.array([u.movement_speed for u in units])
+        movement_speed = np.repeat(movement_speed[:, np.newaxis], len(enemies), axis=1)
+
+        time_to_attack = np.divide(distances, movement_speed)
+        return time_to_attack
 
     def _optimal_targeting(self) -> Mapping[Unit, Unit]:
         units = self.combatants
@@ -429,7 +392,9 @@ class CombatAction:
         if not any(units) or not any(enemies):
             return {}
 
-        cost = self._targeting_cost(units, enemies)
+        # cost = self._time_to_attack(units, enemies) + self._time_to_kill(units, enemies)
+        cost = self.time_to_attack + self.time_to_kill
+        cost = np.nan_to_num(cost, nan=np.inf)
 
         if self.state.knowledge.is_micro_map:
             max_assigned = None
