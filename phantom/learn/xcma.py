f3ec05e07b7af3a62904f3ebe0cb888c0edd1f95
Volker Schmidt <volker.schmidt91@gmail.com>
2026-01-25 07:20:47 +0100

    refactor xcma

--- phantom/learn/xcma.py
+++ phantom/learn/xcma.py
@@ -1,53 +1,58 @@
 import numpy as np
-from scipy.linalg import expm
+from numpy.linalg import cond, norm
+from scipy.linalg import expm, qr
+from scipy.stats import chi
 
 
 class XCMA:
-    def __init__(self, x0, sigma0, lam=None):
+    def __init__(self, x0, sigma0, pc=None, ps=None):
         self.d = len(x0)
-        self.m = np.array(x0, float)
-        self.sig = sigma0
-        self.lam = lam or (4 + int(3 * np.log(self.d)))
-        self.mu = self.lam // 2
-        self.w = np.log(self.mu + 0.5) - np.log(np.arange(1, self.mu + 1))
-        self.w /= self.w.sum()
-        self.mueff = 1 / (self.w**2).sum()
+        self.loc = np.array(x0, float)
+        scale = np.asarray(sigma0, dtype=float)
+        if scale.ndim == 0:
+            scale = np.repeat(scale, self.d)
+        if scale.ndim == 1:
+            scale = np.diag(scale)
+        self.scale = scale
+        self.sigma = 1.0
+        self.pc = pc or np.zeros(self.d)
+        self.ps = ps or np.zeros(self.d)
 
-        self.cc = (4 + self.mueff / self.d) / (self.d + 4 + 2 * self.mueff / self.d)
-        self.cs = (self.mueff + 2) / (self.d + self.mueff + 5)
-        self.c1 = 2 / ((self.d + 1.3) ** 2 + self.mueff)
-        self.cmu = min(1 - self.c1, 2 * (self.mueff - 2 + 1 / self.mueff) / ((self.d + 2) ** 2 + self.mueff))
-        self.ds = 1 + self.cs + 2 * max(0, np.sqrt((self.mueff - 1) / (self.d + 1)) - 1) + self.cs
-
-        self.A = np.eye(self.d)
-        self.pc = np.zeros(self.d)
-        self.ps = np.zeros(self.d)
-        self.gen = 0
-
-    def ask(self, rng=None):
+    def ask(self, num_samples=None, rng=None):
+        n = num_samples or (4 + int(3 * np.log(self.d)))
         rng = rng or np.random.default_rng()
-        self.z = rng.standard_normal((self.d, self.lam))
-        return self.m[:, None] + self.sig * (self.A @ self.z)
+        n2 = n // 2
+        z2 = rng.standard_normal((self.d, n2))
+        if z2.shape[1] <= z2.shape[0]:
+            z2 = qr(z2)[0] * chi.mean(n)
+        z = np.hstack([z2, -z2])
+        x = self.loc[:, None] + self.sigma * (self.scale @ z)
+        return z, x
 
-    def tell(self, fitnesses):
-        idx = np.argsort(fitnesses)[: self.mu]
-        z_sel = self.z[:, idx]
-        z_w = z_sel @ self.w
-
-        self.m += self.sig * (self.A @ z_w)
-        self.ps = (1 - self.cs) * self.ps + np.sqrt(self.cs * (2 - self.cs) * self.mueff) * z_w
-
-        hs = (
-            1
-            if np.linalg.norm(self.ps) / np.sqrt(1 - (1 - self.cs) ** (2 * (self.gen + 1))) < 1.4 + 2 / (self.d + 1)
-            else 0
+    def tell(self, samples, ranking, epsilon=1e-10):
+        n = samples.shape[1]
+        w = np.maximum(0, np.log(n / 2 + 1) - np.log(np.arange(1, n + 1)))
+        w = w / np.sum(w) - 1 / n
+        mueff = (norm(w, 1) / norm(w, 2)) ** 2
+        cc = (4 + mueff / self.d) / (self.d + 4 + 2 * mueff / self.d)
+        cs = (mueff + 2) / (self.d + mueff + 5)
+        c1 = 2 / ((self.d + 1.3) ** 2 + mueff)
+        cmu = min(1 - c1, 2 * (mueff - 2 + 1 / mueff) / ((self.d + 2) ** 2 + mueff))
+        ds = 1 + cs + 2 * max(0, np.sqrt((mueff - 1) / (self.d + 1)) - 1)
+        z = samples[:, ranking]
+        grad_mu = z @ w
+        grad_scale = (z * w) @ z.T
+        self.ps = (1 - cs) * self.ps + np.sqrt(cs * (2 - cs) * mueff) * grad_mu
+        hs = 1 if np.linalg.norm(self.ps) < 1.4 + 2 / (self.d + 1) else 0
+        self.pc = (1 - cc) * self.pc + hs * np.sqrt(cc * (2 - cc) * mueff) * grad_mu
+        step_scale = c1 * (np.outer(self.pc, self.pc) - np.eye(self.d)) + cmu * grad_scale
+        self.scale = self.scale @ expm(0.5 * step_scale)
+        self.sigma *= np.exp(cs / ds * (np.linalg.norm(self.ps) / chi.mean(self.d) - 1))
+        step_loc = self.sigma * (self.scale @ grad_mu)
+        self.loc += step_loc
+        return (
+            self.sigma < epsilon
+            or np.linalg.norm(self.scale, ord=2) < epsilon
+            or np.linalg.norm(step_loc, ord=2) < epsilon
+            or np.linalg.cond(self.scale) > 1 / epsilon
         )
-        self.pc = (1 - self.cc) * self.pc + hs * np.sqrt(self.cc * (2 - self.cc) * self.mueff) * z_w
-
-        G = self.c1 * (np.outer(self.pc, self.pc) - np.eye(self.d)) + self.cmu * (
-            (z_sel * self.w) @ z_sel.T - np.eye(self.d)
-        )
-
-        self.A = self.A @ expm(0.5 * G)
-        self.sig *= np.exp(self.cs / self.ds * (np.linalg.norm(self.ps) / np.sqrt(self.d) - 1))
-        self.gen += 1
