854a6fd2993650b686bf3766fd7ac7d47d9bf8a6
Volker Schmidt <volker.schmidt91@gmail.com>
2026-02-21 04:43:33 +0100

    cleanup highspy wrapper

--- phantom/common/distribute/cpg/__init__.py
+++
@@ -1,6 +0,0 @@
-from phantom.common.distribute.cpg.solver import CpgSolver, get_cpg_solver
-
-__all__ = [
-    "CpgSolver",
-    "get_cpg_solver",
-]
--- phantom/common/distribute/hs/__init__.py
+++
@@ -1,6 +0,0 @@
-from phantom.common.distribute.hs.solver import HighsPySolver, get_hs_solver
-
-__all__ = [
-    "HighsPySolver",
-    "get_hs_solver",
-]
